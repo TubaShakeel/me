57cee9123c7bd5e91ee99a94287bf544dcbbf18d
TubaShakeel <135033839+TubaShakeel@users.noreply.github.com>
2023-08-11 12:20:49 +1000

    WEEK 4

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -29,18 +29,26 @@ def get_some_details():
     Return a new dictionary that just has the last name, password, and the
     number you get when you add the postcode to the id-value.
     TIP: Make sure that you add the numbers, not concatinate the strings.
-         E.g. 2000 + 3000 = 5000 not 20003000
+    E.g. 2000 + 3000 = 5000 not 20003000
     TIP: Keep a close eye on the format you get back. JSON is nested, so you
-         might need to go deep. E.g to get the name title you would need to:
-         data["results"][0]["name"]["title"]
-         Look out for the type of brackets. [] means list and {} means
-         dictionary, you'll need integer indeces for lists, and named keys for
-         dictionaries.
+    might need to go deep. E.g to get the name title you would need to:
+    data["results"][0]["name"]["title"]
+    Look out for the type of brackets. [] means list and {} means
+    dictionary, you'll need integer indeces for lists, and named keys for
+    dictionaries.
     """
     json_data = open(LOCAL + "/lazyduck.json").read()
 
     data = json.loads(json_data)
-    return {"lastName": None, "password": None, "postcodePlusID": None}
+
+    # Extracting the required information
+    last_name = data["results"][0]["name"]["last"]
+    password = data["results"][0]["login"]["password"]
+    postcode = int(data["results"][0]["location"]["postcode"])
+    id_value = int(data["results"][0]["id"]["value"])
+    postcode_plus_id = postcode + id_value
+
+    return {"lastName": last_name, "password": password, "postcodePlusID": postcode_plus_id}
 
 
 def wordy_pyramid():
@@ -77,7 +85,21 @@ def wordy_pyramid():
     ]
     TIP: to add an argument to a URL, use: ?argName=argVal e.g. &wordlength=
     """
-    pyramid = []
+        def wordy_pyramid():
+    
+    base_url = Literal["https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength="]
+    min_length = 3
+    max_length = 20
+    step = 2
+
+    for length in range(min_length, max_length + 1, step):
+        response = requests.get(base_url + str(length))
+        
+        if response.status_code == 200:
+            word = response.text.strip()
+            pyramid.append(word)
+        else:
+            pyramid.append(None)  # Add a placeholder if request fails
 
     return pyramid
 
@@ -91,18 +113,37 @@ def pokedex(low=1, high=5):
     Parse the json and extract the values needed.
 
     TIP: reading json can someimes be a bit confusing. Use a tool like
-         http://www.jsoneditoronline.org/ to help you see what's going on.
+        http://www.jsoneditoronline.org/ to help you see what's going on.
     TIP: these long json accessors base["thing"]["otherThing"] and so on, can
-         get very long. If you are accessing a thing often, assign it to a
-         variable and then future access will be easier.
+    get very long. If you are accessing a thing often, assign it to a
+        variable and then future access will be easier.
     """
     id = 5
     url = f"https://pokeapi.co/api/v2/pokemon/{id}"
     r = requests.get(url)
     if r.status_code is 200:
         the_json = json.loads(r.text)
+def pokedex(low=1, high=5):
+    tallest_pokemon = {"name": None, "weight": None, "height": None}
+    
+    max_height = 0
 
-    return {"name": None, "weight": None, "height": None}
+    for id in range(low, high + 1):
+        url = f"https://pokeapi.co/api/v2/pokemon/{id}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            pokemon_data = json.loads(response.text)
+
+            height = pokemon_data["height"]
+            if height > max_height:
+                max_height = height
+                tallest_pokemon["name"] = pokemon_data["name"]
+                tallest_pokemon["weight"] = pokemon_data["weight"]
+                tallest_pokemon["height"] = height
+
+    return tallest_pokemon
+
 
 
 def diarist():
@@ -114,15 +155,28 @@ def diarist():
 
     TIP: you need to write a string, so you'll need to cast your number
     TIP: Trispokedovetiles(laser).gcode uses windows style line endings. CRLF
-         not just LF like unix does now. If your comparison is failing this
-         might be why. Try in rather than == and that might help.
+        not just LF like unix does now. If your comparison is failing this
+        might be why. Try in rather than == and that might help.
     TIP: remember to commit 'lasers.pew' and push it to your repo, otherwise
-         the test will have nothing to look at.
+        the test will have nothing to look at.
     TIP: this might come in handy if you need to hack a 3d print file in the future.
 
     NOTE: this function doesn't return anything. It has the _side effect_ of modifying the file system
     """
-    pass
+    def diarist():
+    # Read the G-code file
+    file_path = "Trispokedovetiles(laser).gcode"
+    with open(file_path, "r") as gcode_file:
+        gcode_content = gcode_file.read()
+
+    # Count the occurrences of the command "M10 P1"
+    command_to_count = "M10 P1"
+    command_count = gcode_content.count(command_to_count)
+
+    # Write the count to 'lasers.pew' file
+    output_file_path = "Set4/lasers.pew"
+    with open(output_file_path, "w") as output_file:
+        output_file.write(str(command_count))
 
 
 if __name__ == "__main__":
